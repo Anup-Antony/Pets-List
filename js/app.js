8fa7131d565361ab3b7ea78a70a98b7f531052d8
Anup-Antony <anupantony88@gmail.com>
2016-09-23 21:57:37 +0530

    Update app.js
    Comments added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 (function () {
+    
     var mod = angular.module("Pets", []);
 
+    /* Initialization of Constants */
     mod.constant("Constants", {
         "serviceUrl": "http://agl-developer-test.azurewebsites.net/people.json",
         "petTypes": {
@@ -10,10 +12,14 @@
         }
     }
     );
+    
+    /* Controller Declaration */
     mod.controller("PetsController", PetsController);
 
+    /* Dependency injection */
     PetsController.$inject = ["$scope", "$http", "Constants", "$filter"];
 
+    /* Controller Definition */
     function PetsController($scope, $http, Constants, $filter) {
         $scope.getPetsData = getPetsData;
         $scope.getCatsOfOwner = getCatsOfOwner;
@@ -22,7 +28,10 @@
         $scope.errorMessage = "";
         $scope.ownersAndTheirPets = [];
         getPetsData();
-
+        
+         /**
+          * Function that gets the owner and their pets data from the server
+          */
         function getPetsData() {
             $scope.isLoading = true;
             $http.get(Constants.serviceUrl)
@@ -40,17 +49,27 @@
 
         }
 
+         /**
+          * Creates an array of cats according to the gender of the owner
+          * @param {String} ownerGender
+          * @returns {Array}
+          */
         function getCatsOfOwner(ownerGender) {
+            
+            // Creates an array of cats alone for every owner (filtering out all other pet types) in the master data set
             var ownerWithCats = $filter('filter')($scope.ownersAndTheirPets, function (owner, index, dataset) {
                 owner.pets = $filter('filter')(owner.pets, function (pet) {
                     return pet.type === Constants.petTypes.cat;
                 });
                 return owner;
             });
+            
+            // Filters out the owners who do not own any pet
             var catsAccordingToOwnerGender = $filter('filter')(ownerWithCats, function(owner, index, dataset) {
                 return owner.gender === ownerGender && owner.pets;
             })
                 
+            // Returns the final list of all cats owned by all the owners of a particular gender
             return catsAccordingToOwnerGender.reduce(function (accum, item) {
                 return accum.concat(item.pets);
             }, []);
